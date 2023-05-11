5ba1d5d6181f8db49ca80cf1304ebcfbc200caba
jcsyj <juanito.csy@gmail.com>
2023-05-11 08:02:38 -0400

    move locators to separate file

--- tests/todo-app.spec.ts
+++ tests/todo-app.spec.ts
@@ -1,6 +1,7 @@
-import { test, expect, type Page, Locator } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { checkNumberOfCompletedTodosInLocalStorage, checkNumberOfTodosInLocalStorage, checkTodosInLocalStorage, createTodos } from '../helper/todo-app';
 import { todo as TODO_ITEMS } from '../fixtures/todo.json';
+import { TODO } from '../locators/todo';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/todomvc');
@@ -14,17 +15,17 @@ test.describe('Create New Todo', () => {
   });
 
   test('should be able to create new items on the page', async ({ page }) => {
-    await expect(page.getByTestId('todo-title')).toHaveText([...TODO_ITEMS], { ignoreCase: false });
+    await expect(page.getByTestId(TODO.title)).toHaveText([...TODO_ITEMS], { ignoreCase: false });
     await checkNumberOfTodosInLocalStorage(page, totalCount);
   });
 
   test('should be able to clear input text when an item is added', async ({ page }) => {
-    await expect(page.locator('input.new-todo')).toBeEmpty();
+    await expect(page.locator(TODO.newInput)).toBeEmpty();
     await checkNumberOfTodosInLocalStorage(page, totalCount);
   });
 
   test('should trim entered text', async ({ page }) => {
-    const toDoTitles = page.getByTestId('todo-title');
+    const toDoTitles = page.getByTestId(TODO.title);
     const newItem = '   Submit Code Challenge!     ';
 
     await createTodos(page, [newItem]);
@@ -32,7 +33,7 @@ test.describe('Create New Todo', () => {
   });
 
   test('should append new items to the bottom of the list', async ({ page }) => {
-    const toDoTitles = page.getByTestId('todo-title');
+    const toDoTitles = page.getByTestId(TODO.title);
     const newItem = 'submit code challenge';
 
     for (let i = 0; i < totalCount; i++) {
@@ -41,7 +42,7 @@ test.describe('Create New Todo', () => {
 
     await createTodos(page, [newItem]);
     await expect(toDoTitles.nth(totalCount)).toHaveText(newItem);
-    await expect(page.getByTestId('todo-count')).toContainText(`${totalCount + 1} items left`);
+    await expect(page.getByTestId(TODO.count)).toContainText(`${totalCount + 1} items left`);
   });
 });
 
@@ -56,26 +57,26 @@ test.describe('Marking as completed', () => {
   });
 
   test('should be able to mark all items as completed', async ({ page }) => {
-    await page.locator('input.toggle-all').check();
+    await page.locator(TODO.toggleAll).check();
 
-    await expect(page.getByTestId('todo-item')).toHaveClass(Array.from(new Array(totalCount), () => 'completed'));
-    await expect(page.getByTestId('todo-count')).toContainText('0 items left');
-    await expect(page.locator('button.clear-completed')).toContainText('Clear completed', { ignoreCase: false });
+    await expect(page.getByTestId(TODO.item)).toHaveClass(Array.from(new Array(totalCount), () => 'completed'));
+    await expect(page.getByTestId(TODO.count)).toContainText('0 items left');
+    await expect(page.locator(TODO.clearCompleted)).toContainText('Clear completed', { ignoreCase: false });
     await checkNumberOfCompletedTodosInLocalStorage(page, totalCount);
   });
 
   test('should allow clearing the completed state back to incomplete', async ({ page }) => {
-    const toggleAll = page.locator('input.toggle-all');
+    const toggleAll = page.locator(TODO.toggleAll);
     await toggleAll.check();
     await toggleAll.uncheck();
 
-    await expect(page.getByTestId('todo-item')).toHaveClass(Array.from(new Array(totalCount), () => ''));
-    await expect(page.getByTestId('todo-count')).toContainText(`${totalCount} items left`);
-    await expect(page.locator('footer.footer')).not.toHaveText('Clear completed', { ignoreCase: false });
+    await expect(page.getByTestId(TODO.item)).toHaveClass(Array.from(new Array(totalCount), () => ''));
+    await expect(page.getByTestId(TODO.count)).toContainText(`${totalCount} items left`);
+    await expect(page.locator(TODO.footer)).not.toHaveText('Clear completed', { ignoreCase: false });
   });
 
   test('should allow marking all as completed with the arrow next to the prompt', async ({ page }) => {
-    const toggles = page.locator('input.toggle');
+    const toggles = page.locator(TODO.toggle);
     const togglesCount = await toggles.count();
 
     expect(togglesCount).toEqual(totalCount);
@@ -85,10 +86,10 @@ test.describe('Marking as completed', () => {
 
       await toggles.nth(i).check();
       await expect(toggles.nth(i)).toBeChecked();
-      await expect(page.getByTestId('todo-count')).toHaveText(itemsLeft == 1 ? `${itemsLeft} item left` : `${itemsLeft} items left`);
+      await expect(page.getByTestId(TODO.count)).toHaveText(itemsLeft == 1 ? `${itemsLeft} item left` : `${itemsLeft} items left`);
     }
 
-    await expect(page.locator('input.toggle-all')).toBeChecked();
+    await expect(page.locator(TODO.toggleAll)).toBeChecked();
   });
 });
 
@@ -99,7 +100,7 @@ test.describe('Editing existing todos', () => {
   });
 
   test('should be able to edit a record', async ({ page }) => {
-    const todoItems = page.getByTestId('todo-item');
+    const todoItems = page.getByTestId(TODO.item);
     const itemToEdit = todoItems.nth(2);
     const newItemTitle = 'discuss the approach and solutions';
     TODO_ITEMS[2] = newItemTitle;
@@ -113,7 +114,7 @@ test.describe('Editing existing todos', () => {
   });
 
   test('should trim entered text', async ({ page }) => {
-    const todoItems = page.getByTestId('todo-item');
+    const todoItems = page.getByTestId(TODO.item);
     const itemToEdit = todoItems.nth(1);
     const newItemTitle = '     Ensure coverage for all items is automated!   ';
     TODO_ITEMS[1] = newItemTitle.trim();
@@ -127,7 +128,7 @@ test.describe('Editing existing todos', () => {
   });
 
   test('should remove the item if the text is cleared', async ({ page }) => {
-    const todoItems = page.getByTestId('todo-item');
+    const todoItems = page.getByTestId(TODO.item);
     const itemToEdit = todoItems.nth(0);
 
     await itemToEdit.dblclick();
@@ -136,11 +137,11 @@ test.describe('Editing existing todos', () => {
 
     await expect(todoItems).toHaveText(TODO_ITEMS.slice(1), { ignoreCase: false });
     await checkNumberOfTodosInLocalStorage(page, totalCount - 1);
-    await expect(page.getByTestId('todo-count')).toHaveText(totalCount - 1 == 1 ? `${totalCount - 1} item left` : `${totalCount - 1} items left`);
+    await expect(page.getByTestId(TODO.count)).toHaveText(totalCount - 1 == 1 ? `${totalCount - 1} item left` : `${totalCount - 1} items left`);
   });
 
   test('should cancel edits on escape', async ({ page }) => {
-    const todoItems = page.getByTestId('todo-item');
+    const todoItems = page.getByTestId(TODO.item);
     const itemToEdit = todoItems.nth(1);
 
     await itemToEdit.dblclick();
@@ -158,19 +159,19 @@ test.describe('Other functions', () => {
   });
 
   test('should disable buttons when editing an item', async ({ page }) => {
-    const todoItems = page.getByTestId('todo-item');
+    const todoItems = page.getByTestId(TODO.item);
     const itemToEdit = todoItems.nth(0);
 
     await itemToEdit.dblclick();
     await expect(itemToEdit.locator('button.destroy')).not.toBeVisible();
-    await expect(itemToEdit.locator('input.toggle')).not.toBeVisible();
+    await expect(itemToEdit.locator(TODO.toggle)).not.toBeVisible();
   });
 
   test('should filter the list on completion by the active or complete filters', async ({ page }) => {
-    const todoItems = page.getByTestId('todo-item');
-    const toggles = page.locator('input.toggle');
-    const activeFilter = page.locator('[href="#/active"]');
-    const completedFilter = page.locator('[href="#/completed"]');
+    const todoItems = page.getByTestId(TODO.item);
+    const toggles = page.locator(TODO.toggle);
+    const activeFilter = page.locator(TODO.active);
+    const completedFilter = page.locator(TODO.completed);
     const index = 1;
     const checkActive = async (items: string[]) => {
       await activeFilter.click();
@@ -186,7 +187,7 @@ test.describe('Other functions', () => {
     };
 
     await toggles.nth(index).check();
-    await expect(page.getByTestId('todo-count')).toHaveText(totalCount - 1 == 1 ? `${totalCount - 1} item left` : `${totalCount - 1} items left`);
+    await expect(page.getByTestId(TODO.count)).toHaveText(totalCount - 1 == 1 ? `${totalCount - 1} item left` : `${totalCount - 1} items left`);
 
     const completedItem = TODO_ITEMS.splice(index, 1);
 
